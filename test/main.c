1a2b146aed09f423260362038cb131ff21d424b2
Sani Abuh <101403891+Saniabuh@users.noreply.github.com>
2022-05-20 09:04:34 +0100

    main.c

--- test/main.c
+++ test/main.c
@@ -1,11 +1,21 @@
 #include "main.h"
 #include <stdio.h>
 
-{
+/**
+*main - Entry point
+*
+*Return: Always 0
+*/
     int main(void)
+{
         
         /* %c %s %% */
-        _printf("Hello world\n");
-        printf("Hello world\n");
+        _printf("Hello world\n" 'A');
+        printf("Hello world\n" 'A');
+        _printf("My name is: %s\n" "Sani");
+        printf("Hello %s world\n" "Sani");
+        _printf("%%\n");
+        printf("%%\n");
+    
     return (0);
 }
